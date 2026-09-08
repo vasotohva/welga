ac5fd30ba1c1e4ceb473200a317d4519eff9a355
vasotohva <vasoto@gmail.com>
2026-09-08 14:27:58 +0300

    Application: add site config template

---
+++ storage/config/site.example.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'name' => 'WELGA',
+    'url' => 'https://welga.com',
+    'default_language' => 'bg',
+    'language_prefixes' => [
+        'bg' => '',
+        'en' => 'en',
+        'de' => 'de',
+    ],
+];
